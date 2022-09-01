253e102e8aea3f4637a7ab6cf6ddeadea317c4a7
abhishekbardolia <abhibardolia@gmail.com>
2022-09-01 10:27:47 +0530

    - initial commit

---
+++ android/app/src/main/kotlin/com/oropocket/movie_db_practical/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.oropocket.movie_db_practical
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:movie_db_practical/screen/bottom_nav_bar.dart';
+import 'package:movie_db_practical/screen/splash/splash_screen.dart';
+import 'package:movie_db_practical/utils/color_constant.dart';
+import 'package:movie_db_practical/utils/constants.dart';
+
+import 'bloc/top_movies_bloc.dart';
+import 'data/service_locator.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  setUpLocator();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Movie database',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        hoverColor: Colors.transparent,
+        highlightColor: Colors.transparent,
+        scaffoldBackgroundColor: ColorConstant.primaryColor,
+        primarySwatch: Colors.blue,
+        splashColor: Colors.transparent,
+        primaryColor: ColorConstant.blue500,
+        textTheme: GoogleFonts.interTextTheme(),
+      ),
+      builder: (context, child) {
+        return MediaQuery(
+          data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+          child: ScrollConfiguration(
+            behavior: NoGlowBehavior(),
+            child: child!,
+          ),
+        );
+      },
+      home: const SplashScreen()
+
+    );
+  }
+}
+
+class NoGlowBehavior extends ScrollBehavior {
+  @override
+  Widget buildViewportChrome(
+      BuildContext context,
+      Widget child,
+      AxisDirection axisDirection,
+      ) {
+    return child;
+  }
+}
